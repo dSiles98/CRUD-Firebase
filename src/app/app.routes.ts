0a1a6154a8ed0fa087caa2cf3e2dfde592d46497
dsiles <dayana.siles@fundacion-jala.org>
2020-07-03 19:53:43 -0400

    Init project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { EditUserResolver } from './edit-user/edit-user.resolver';
+
+export const rootRouterConfig: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'new-user', component: NewUserComponent },
+  { path: 'details/akRh7lZLdJxkvLp2Uev1', component: EditUserComponent, resolve:{data : EditUserResolver} }
+];
